d91171aad2c9cfd188faaa7bf662133cb19c708f
Сергей <esinsergej@gmail.com>
2017-11-22 08:44:34 +0300

    start

---
+++ 01_Murray-Davis/Murray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Murray_Davis
+{
+    static class Murray
+    {
+        //static double solid;
+        //static double raster;
+
+        static public double Density(double dotsol, double dotras)
+        {
+            if (dotras == 0) return 0;
+            if (dotsol < dotras) return 0; // заглушка для исключения
+
+            return 100 * ((1 - Math.Pow(10, -dotras)) / (1 - Math.Pow(10, -dotsol)));
+        }
+
+
+    }
+}
---
+++ 01_Murray-Davis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Murray_Davis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Murray.Density(1.7, 1.9));
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
